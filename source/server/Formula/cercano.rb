9b9e728de058f315e26cc3dc7f1b8ab92935b118
bryan costanich <b@wowzer.net>
2026-03-26 22:08:16 -0700

    chore: Bump local formula to v0.8.0

--- source/server/Formula/cercano.rb
+++ source/server/Formula/cercano.rb
@@ -1,22 +1,22 @@
 class Cercano < Formula
   desc "AI-powered development tool with local/cloud model routing"
   homepage "https://github.com/bryancostanich/Cercano"
-  version "0.7.2"
+  version "0.8.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bryancostanich/Cercano/releases/download/v#{version}/cercano-darwin-arm64"
-      sha256 "853792dd2865e71d3f91ec12d09c7677af80b70320140a2594c3fa0f5f7e3e77"
+      sha256 "ac6bb016484cd8cca9b767101d9d0d6e605135f77edc3d8ff3257304abc9c4b4"
     else
       url "https://github.com/bryancostanich/Cercano/releases/download/v#{version}/cercano-darwin-amd64"
-      sha256 "5ae1f4d09e653964bf22f1d582e4a5f9d67eebf1d3903245e29aba7c3bbbb64b"
+      sha256 "24e8d6b7a0cf8cef3b40af7b06d249394154ae26e5dc1559c2654af935851ad3"
     end
   end
 
   on_linux do
     url "https://github.com/bryancostanich/Cercano/releases/download/v#{version}/cercano-linux-amd64"
-    sha256 "0a26a8bb456a790a3b83833961cdae105cea10c1c5a5df1e0f07fe54fda08420"
+    sha256 "8b7d65071917ca411c0d5416b1298fc6f6d59a0c6a9cf1d927f338094f58d482"
   end
 
   def install
